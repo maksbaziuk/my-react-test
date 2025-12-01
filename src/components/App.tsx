283ec9bf75c21fb65271fb84aa2198e813dbdd66
BMM <m.bazyk@gmail.com>
2025-12-01 19:07:43 +0200

    add form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,63 +1,33 @@
-// // src/components/App.tsx
-
-// import Product from "./Product";
-
-// export default function App() {
-//   return (
-//     <>
-//       <h1>Best selling</h1>
-//       <Product
-//         name="Tacos With Lime"
-//         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-//         price={10.99}
-//       />
-//       <Product
-//         name="Fries and Burger"
-//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
-//         price={14.29}
-//       />
-//     </>
-//   );
-// }
-
-// src/components/App.tsx
-
-// import Alert from "./Alert";
-
-// export default function App() {
-//   return (
-//     <>
-//       <Alert />
-//       <Alert type="success"  />
-//       <Alert type="error" />
-//     </>
-//   );
-// }
-
-// src/components/App.tsx
-
-// import Button from "./Button";
-
-// export default function App() {
-//   return (
-//     <>
-//       <Button variant="primary" text="Login" />
-//       <Button variant="secondary" text="Follow" />
-//     </>
-//   );
-// }
-
-// 1. Імпортуємо функцію useState
 import { useState } from "react";
+import SearchForm from "./SearchForm";
+import type { Article } from "../types/article";
+import ArticleList from "./ArticleList";
+import { fetchArticles } from "../services/articleService";
 
 export default function App() {
-  // 2. Оголошуємо стан clicks
-  const [clicks, setClicks] = useState(0);
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const handleClick = () => {
-    // 3. Використовуємо setClicks для зміни стану clicks
-    setClicks(clicks + 1);
+  const handleSearch = async (topic: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const data = await fetchArticles(topic);
+      setArticles(data);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  return <button onClick={handleClick}>Current: {clicks}</button>;
+  return (
+    <div>
+      <SearchForm onSubmit={handleSearch} />
+      {isLoading && <p>Loading data, please wait...</p>}
+      {isError && <p>Whoops, something went wrong! Please try again!</p>}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
 }
