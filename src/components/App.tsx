bf2698c9c7fb7e33e93c1ec20a523889b2510c17
BMM <m.bazyk@gmail.com>
2025-12-09 17:39:47 +0200

    add

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import SearchForm from "./SearchForm";
+import ArticleList from "./ArticleList";
+import { fetchArticles } from "../services/articleService";
 
 export default function App() {
-  const [clicks, setClicks] = useState(() => {
-    const savedClicks = localStorage.getItem("saved-clicks");
-    if (savedClicks !== null) {
-      return JSON.parse(savedClicks);
-    }
-    return 0;
+  const [topic, setTopic] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["articles", topic, currentPage],
+    queryFn: () => fetchArticles(topic, currentPage),
+    enabled: topic !== "",
+    placeholderData: keepPreviousData,
   });
 
-  useEffect(() => {
-    localStorage.setItem("saved-clicks", JSON.stringify(clicks));
-  }, [clicks]);
+  const totalPages = data?.nbPages ?? 0;
+
+  const handleSearch = async (newTopic: string) => {
+    setTopic(newTopic);
+    setCurrentPage(1);
+  };
 
   return (
-    <div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
+    <>
+      <SearchForm onSubmit={handleSearch} />
+      <p>
+        Current page {currentPage} | Total pages {totalPages}
+      </p>
+      <button
+        onClick={() => setCurrentPage(Math.max(currentPage - 1, 1))}
+        disabled={currentPage === 1}
+      >
+        Previous
       </button>
-      <button onClick={() => setClicks(0)}>Reset</button>
-    </div>
+      <button
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={currentPage >= totalPages}
+      >
+        Next
+      </button>
+      {isLoading && <p>Loading data, please wait...</p>}
+      {isError && <p>Whoops, something went wrong! Please try again!</p>}
+      {data && data.hits.length > 0 && <ArticleList items={data.hits} />}
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-// src/main.tsx
-
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import "./index.css";
 import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
