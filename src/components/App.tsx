546d1db17d53faf8d3c2c46c1f9159e50106f112
BMM <m.bazyk@gmail.com>
2025-12-18 16:23:23 +0200

    add

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,60 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
+import axios from "axios";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface FetchPostsResponse {
+  posts: Post[];
+}
+
+export const fetchPosts = async (searchText: string) => {
+  const res = await axios.get<FetchPostsResponse>(
+    "https://dummyjson.com/posts/search",
+    {
+      params: {
+        q: searchText,
+      },
+    }
+  );
+  return res.data.posts;
+};
 
 export default function App() {
-  const [text, setText] = useState("hello");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const handleChange = useDebouncedCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value),
-    1000
+  const { data: posts, isFetching } = useQuery({
+    queryKey: ["posts", searchQuery],
+    queryFn: () => fetchPosts(searchQuery),
+    placeholderData: keepPreviousData,
+  });
+
+  const updateSearchQuery = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value),
+    300
   );
-
-  useEffect(() => {
-    console.log(`Make HTTP request with: ${text}`);
-  }, [text]);
 
   return (
     <>
-      <input type="text" defaultValue={text} onChange={handleChange} />
-      <p>Text value: {text}</p>
+      <input
+        type="text"
+        defaultValue={searchQuery}
+        onChange={updateSearchQuery}
+        placeholder="Search posts"
+      />
+      {isFetching && <div>Loading posts...</div>}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
